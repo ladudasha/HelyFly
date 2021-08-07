6497e88582ca9f197896d43c6908f30137d43030
ladudasha <darya.gameza@gmail.com>
2021-08-07 23:23:47 +0300

    section maps start

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,13 @@
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
+// $(document).ready(function() {
+//     $('.maps__slider').slick({
+//         dots: true
+//     });
+
+
+// });
+
+$(document).ready(function() {
+    $('.maps__slider').slick({
+        setting - name: setting - value
+    });
+});
